4a9e3acfd7d653804305c9697a03d6e97255673d
AlexNosanchuk <106674285+AlexNosanchuk@users.noreply.github.com>
2022-06-02 22:18:42 +0300

    Create 1.9.c

---
+++ 1.9.c
@@ -0,0 +1,15 @@
+/* task 1.9c */
+
+#include <stdio.h>
+
+int main(){
+    
+    float a;
+    printf("a=");
+    scanf("%f\n", &a);
+    printf("- %f - %f - %f", a,a,a);
+    printf("%f | %f | %f", a,a,a);
+    printf("- %f - %f - %f", a,a,a);
+    return 0;
+    
+}
