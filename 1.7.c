b8b8f54e44a7f36f76c94a0b736602b5db26790d
AlexNosanchuk <106674285+AlexNosanchuk@users.noreply.github.com>
2022-06-02 22:16:18 +0300

    Create 1.7.c

---
+++ 1.7.c
@@ -0,0 +1,15 @@
+#include <math.h>
+
+int main(){
+    
+    float x;
+    printf("x=");
+    scanf("%f", &x);
+    int y;
+    y = (int)x;
+    float y1;
+    y1 = x-y;
+    printf("%d\n", y);
+    printf("%f", y1);
+    
+}
